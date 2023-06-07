50b78e331ff0e23eec2175801bc0665ea92137cf
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-07 08:34:10 +0000

    Update Navs.ts

--- store/Navs.ts
+++ store/Navs.ts
@@ -8,11 +8,10 @@ interface Navs {
   topNav: HTMLElement | null;
   bottomNav: HTMLElement | null;
   scrolled: boolean;
-prevScrollPos: Number | null
+  prevScrollPos: Number | null;
 
   scrollEventHandler: null;
 }
-
 
 export const useNavs = defineStore("navs", {
   state: () => ({
@@ -21,34 +20,35 @@ export const useNavs = defineStore("navs", {
     bottomNav: null,
     scrolled: false,
     scrollEventHandler: null,
-    
-    prevScrollPos: null
+
+    prevScrollPos: null,
   }),
 
   actions: {
     setScrollEvent() {
       this.prevScrollPos = this.container?.pageYOffset;
       this.scrollEventHandler = this.handler.bind(this);
-      this.container?.addEventListener("scroll", this.scrollEventHandler, false);
-      
+      this.container?.addEventListener(
+        "scroll",
+        this.scrollEventHandler,
+        false
+      );
+
       if (this.prevScrollPos < 22) {
         this.topNav?.classList.add("FullTrans");
       }
-      // else {
-      //   this.topNav?.classList.remove("FullTrans")
-      // }
     },
-    
+
     handler(e) {
       if (e.isTrusted) {
         var subsequentScrollPos = this.container?.pageYOffset;
-        
+
         if (subsequentScrollPos > 200) {
           this.scrolled = true;
         } else {
           this.scrolled = false;
         }
-        
+
         if (subsequentScrollPos > 200) {
           const diff = subsequentScrollPos - this.prevScrollPos;
           if (subsequentScrollPos > this.prevScrollPos) {
@@ -60,30 +60,32 @@ export const useNavs = defineStore("navs", {
             this.topNav?.classList.remove("rotated");
             this.prevScrollPos = subsequentScrollPos;
           }
-        } 
-        else {
-          if(subsequentScrollPos < 22) {
+        } else {
+          if (subsequentScrollPos < 22) {
             this.topNav?.classList.add("FullTrans");
-          } 
-          else {
-            this.topNav?.classList.remove("FullTrans")
+          } else {
+            this.topNav?.classList.remove("FullTrans");
           }
         }
       }
     },
-    
+
     removeScrollEvent() {
-      this.container?.removeEventListener("scroll", this.scrollEventHandler, false);
+      this.container?.removeEventListener(
+        "scroll",
+        this.scrollEventHandler,
+        false
+      );
     },
-    
+
     setContainer(value) {
       this.container = value;
     },
-    
+
     setTopNav(value) {
       this.topNav = value;
     },
-    
+
     setBottomNav(value) {
       this.bottomNav = value;
     },
