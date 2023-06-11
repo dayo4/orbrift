f07b2ed24f0baf966b3d276d6a08cca1a62034cd
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-11 09:19:55 +0000

    Update about.vue and create.vue

--- pages/about.vue
+++ pages/about.vue
@@ -6,38 +6,140 @@
     </template>
 
     <template #WrapperBody>
-      <section class="flex j-c-center">
+      <section class="AB_Cont flex j-c-center">
         <div class="xs5 sm4 md3 mb-5">
           <img src="/defaults/pgs/orbrift_ab2.jpg" draggable="false" />
         </div>
       </section>
 
-      <section class="flex j-c-center wrap">
-        <section
-          v-for="(QA, i) in QueAns"
-          :key="i"
-          class="xs12 sm10 md10 mb-10"
-        >
-          <!-- Question -->
-          <div class="Ques br2 noselect">
-            <div class="Body pl-10 br3">
-              <div class="bold-4 p-2">{{ QA.Q }}</div>
-              <span class="Img br5">
-                <img class="br5" src="/defaults/usr/a.jpg" draggable="false" />
-              </span>
-            </div>
-          </div>
+      <section class="flex j-c-center">
+        <section class="xs11 sm9 md8 mb-10">
+          <section>
+            <h2 class="AB_Head">About Orbrift</h2>
+            <p>
+              Welcome to Orbrift, a leading web development team committed to
+              providing exceptional digital solutions for designers, businesses,
+              and creative-minded individuals. Our mission is to empower brands
+              that reach out to us with captivating web experiences that elevate their online
+              presence and drive tangible results.
+            </p>
+          </section>
 
-          <!-- Answer -->
-          <div class="Ans br2 noselect" v-for="(ans, i) in QA.A" :key="i">
-            <div class="Body bg-grey-5 br3">
-              <div v-html="ans" class="p-2 pl-2"></div>
-            </div>
-          </div>
+          <section>
+            <h2 class="AB_Head">Our Expertise</h2>
+            <p>A talented team of software developer, designer, and strategists, combining our expertise to offer a comprehensive range of services.</p>
+            <article>
+              <h3 class="AB_SubHead">Custom Web Design and Development</h3>
+              <p>
+                We craft stunning, bespoke websites that blend captivating
+                design with seamless functionality. Our team meticulously
+                translates your vision into a pixel-perfect digital reality,
+                ensuring an immersive user experience and strong brand
+                representation.
+              </p>
+            </article>
+
+            <article>
+              <h3 class="AB_SubHead">E-Commerce Solutions</h3>
+              <p>
+                Drive your online business forward with our robust and scalable
+                e-commerce solutions. We leverage industry-leading platforms and
+                technologies to build secure, user-friendly online stores that
+                optimize conversions and enhance customer satisfaction.
+              </p>
+            </article>
+
+            <article>
+              <h3 class="AB_SubHead">Web Application Development</h3>
+              <p>
+                We excel in developing powerful web applications that streamline
+                your business operations and deliver exceptional user
+                experiences. From sophisticated enterprise solutions to
+                innovative startups, our expertise spans across various
+                industries and technologies.
+              </p>
+            </article>
+
+            <article>
+              <h3 class="AB_SubHead">Graphics and Product Design</h3>
+              <p>
+                From visually striking logos to compelling marketing collateral, our graphic design services help you create a cohesive and impactful brand identity. We specialize in designing engaging visuals that resonate with your target audience and leave a lasting impression.
+              </p>
+            </article>
+
+            <article>
+              <h3 class="AB_SubHead">Mobile-Friendly and Responsive Design</h3>
+              <p>
+                In today's mobile-centric world, we prioritize responsive design
+                to ensure your website looks flawless and performs seamlessly
+                across all devices. Our mobile-first approach guarantees an
+                optimal user experience, driving engagement and conversions.
+              </p>
+            </article>
+
+            <article>
+              <h3 class="AB_SubHead">Search Engine Optimization (SEO)</h3>
+              <p>
+                Our SEO experts employ proven strategies to optimize your
+                website for search engines, improving its visibility and driving
+                organic traffic. We focus on technical optimizations, content
+                strategies, and link building to enhance your online presence
+                and boost rankings.
+              </p>
+            </article>
+          </section>
+
+          <section>
+            <h2 class="AB_Head">Our Approach</h2>
+            <ul>
+              <li>
+                <strong class="t-blue-grey--1">Strategic Collaboration:</strong>
+                We believe in forging strong relationships with brands that reach out to us. We
+                work closely with you, engaging in open communication and active
+                collaboration throughout the entire development process.
+              </li>
+              <li>
+                <strong class="t-blue-grey--1">Customized Solutions:</strong>
+We understand that each client has unique requirements. That's why we take a tailored approach to deliver solutions that align with your specific goals and objectives.
+
+              </li>
+              <li>
+                <strong class="t-blue-grey--1">Strategic Guidance:</strong> We
+                offer objective guidance to address common challenges faced by
+                websites and digital businesses. Our team brings valuable
+                insights and expertise to the table, assisting you in making
+                informed decisions to maximize your online success.
+              </li>
+              <li>
+                <strong class="t-blue-grey--1"
+                  >Innovation and Technology:</strong
+                >
+                We stay at the forefront of emerging technologies and industry
+                trends, enabling us to deliver solutions that are future-proof
+                and equipped with the latest advancements.
+              </li>
+              <li>
+                <strong class="t-blue-grey--1">Quality Assurance:</strong> Our
+                rigorous quality assurance processes ensure that every aspect of
+                your website or application is thoroughly tested, resulting in a
+                flawless user experience and optimal performance.
+              </li>
+            </ul>
+          </section>
+
+          <section>
+            <h2 class="AB_Head">Let's Transform Your Online Presence</h2>
+            <p>
+              Are you ready to elevate your brand's digital presence? Contact
+              our team today to discuss your web development goals. Together,
+              we'll embark on a journey to create a captivating online
+              experience that sets you apart from the competition and drives
+              measurable success.
+            </p>
+          </section>
         </section>
       </section>
     </template>
-
   </GlobalWrapper>
 </template>
 <script lang="ts">
@@ -47,90 +149,24 @@ export default {
 
     useSeoMeta($myMetaInfo({ title: "About | orbrift" }));
 
-    const QueAns = [
-      {
-        Q: "About Orbrift!",
-        A: [
-          `<p>Hello! With orbrift you get unique UI designs, web applications development and other web solutions specifically targeted at helping designers and other creative minded setups likewise in profitably presenting their crafts to their target markets. You are delivered professional, custom and result-oriented web development services while also getting access to objective guidance in solving common websites-related business challenges.</p>
-          <p>For more details <a href="/adedayo-adeniyi" class="t-blue bg-grey-3 cursor-pointer br2 p-2 no-deco" target="_blank">Check out my profile.</a></p>`,
-        ],
-      },
-      {
-        Q: "About the products and services!",
-        A: [
-          `<p>You see, it is a dynamic and fast-changing world, everyone here and there is looking to create a customized virtual identity either for themselves or their businesses to boost their reach beyond their local environment. And when they do, they need someone who would do it to a high standard, engineered to their taste. This is where I come in!</p>
-          <p>Everyone and every business deserves a virtual online profile that not only stands out but also fulfills its intended purpose, and I specialize in providing that.</p>
-          <p class="mb-1">From creating regular SEO responsive webpages to implementing complex business and functional ui logic for web technologies. I am versed in presenting a genuine design and overall development services. In my time doing this, I've worked physically and remotely with a number of people building their projects.</p>
-          <p><b>What kind of website or web application do you need now?</b> Personal, for a hobby? for your business or vocation?. Or just need someone to work with you in building your next big project. I'll be happy to get onboard in bringing your ideas to live.</p> 
-          <p>We can discuss the detail <a href="/contact" class="t-blue bg-grey-3 cursor-pointer br2 p-2 no-deco" target="_blank">here. Send a message.</a></p>`,
-        ],
-      },
-      // {
-      //   Q: "Single Page web Applications(SPAs) and Progressive web Applications(PWAs)",
-      //   A: [
-      //     `<p>SPAs/PWAs are web apps that give you a similar mobile apps experience on the web without the need to download and install on your device. Progressive, faster navigation, fully customized, and no boring loadings between pages...</p>
-
-      //     <p>Among others built, a good example is <span class="t-blue bg-grey-3 cursor-pointer br2 p-2 no-deco">orbrift.com</span>.</p>
-      //     <p>Unlike most traditional websites setup on wordpress and the likes, SPAs are most often more custom built for your specific purpose and will have better overall user experience, without requiring much more resources.<p>
-      //     <p>Likewise I also create wordpress websites if it's what you need, and there are ways to still take advantage of it's CMS functionalities in SPAs. Whether personal or ecommerce websites.<p>
-      //     `,
-      //   ],
-      // },
-      //..
-    ];
-
-    return {
-      QueAns,
-    };
+    return {};
   },
 };
 </script>
 <style lang="scss" scoped>
-/* Question & Answer Styling*/
-.Ques,
-.Ans {
-  position: relative;
-
-  & .Img {
-    position: absolute;
-    top: -18px;
-    height: 50px;
-    width: 50px;
-    & img {
-      width: 100%;
-      height: 100%;
-    }
+.AB_Cont {
+  & p {
+    font-size: 16px;
   }
 }
-
-.Ques {
-  margin: 25px 50px 10px 5px;
-  & .Img {
-    background-color: $sec-color;
-    border: solid 3px $sec-color-transparent-more;
-  }
-  & .Body {
-        background-color: $sec-color-transparent;
-        color:$pri-color;
-    border-bottom: solid $sec-color-transparent-more 3px;
-  }
-  & .Img {
-    left: -2px;
-  }
+.AB_Head {
+  color: $blue-grey--1
 }
-
-.Ans {
-  margin: 5px;
-  & .Body {
-    font-size: 20px;
-    letter-spacing: 0.2px;
-    border-bottom: solid $sec-color-transparent-more 3px;
-  }
-}
-
-@include xs-only {
-  .Ques {
-    margin-right: 35px;
-  }
+.AB_SubHead {
+  padding: 10px;
+  background-color: $sec-color-transparent;
+  color: $pri-color;
+  border-bottom: solid $sec-color-transparent-more 3px;
+  border-radius: 10px;
 }
 </style>
