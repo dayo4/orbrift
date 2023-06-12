0f96dc9de9ceb991362bd7f234823c4defc80ad7
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-12 02:33:41 +0000

    added my full about page

--- pages/about.vue
+++ pages/about.vue
@@ -15,136 +15,181 @@
       <section class="flex j-c-center">
         <section class="xs11 sm9 md8 mb-10">
           <section>
-            <h2 class="AB_Head">About Orbrift</h2>
+            <h2 class="AB_Head">About Me</h2>
             <p>
-              Welcome to Orbrift, a leading web development team committed to
-              providing exceptional digital solutions for designers, businesses,
-              and creative-minded individuals. Our mission is to empower brands
-              that reach out to us with captivating web experiences that elevate
-              their online presence and drive tangible results.
+              I'm an enthusiastic explorer, driven by a deep curiosity for
+              design and a desire to explore the secrets of technology. With an
+              extensive understanding of the principles of Node.js, Databases,
+              User Interface (UI), User Experience (UX), and Application
+              Programming Interfaces (APIs), I thrive on creating exceptional
+              digital experiences.
+            </p>
+            <p>
+              Throughout my journey, I've showcased my expertise by crafting
+              modern, cutting-edge websites, stunning designs, and robust
+              Single-Page Applications (SPAs) and Progressive Web Applications
+              (PWAs). By leveraging JavaScript frameworks like Vue.js and
+              React.js, I've developed innovative solutions that seamlessly
+              integrate with modern E-commerce(Online Store) platforms and
+              Content Management Systems (CMS). My proficiency extends to
+              managing Google Cloud Platforms (GCP) and utilizing Linux-based
+              cloud hosting services, ensuring high-performance applications.
+            </p>
+            <p>
+              However, my true passion lies in creating user-centric solutions
+              that captivate and engage. I believe in delivering more than just
+              technical excellence; I strive to enhance user experiences,
+              improve efficiency, and drive business growth. By applying my deep
+              understanding of UI/UX principles and advanced JavaScript
+              frameworks, I develop intuitive, visually appealing websites and
+              applications optimized for top-notch performance. Through seamless
+              integrations with E-commerce platforms and CMS solutions, I
+              empower businesses to streamline their operations, expand their
+              reach, and achieve online success.
+            </p>
+            <p>
+              Beyond my technical endeavors, I find immense joy in engaging in
+              intuitive conversations and exploring the abstract design of
+              nature. I'm constantly inspired by how these principles shape the
+              architecture of our digital world. It's this drive for precision
+              that fuels my passion for every project I undertake.
             </p>
           </section>
 
           <section>
-            <h2 class="AB_Head">Our Expertise</h2>
+            <h2 class="AB_Head">My Expertise</h2>
             <p>
-              A talented team of software developer, designer, and strategists,
-              combining our expertise to offer a comprehensive range of
-              services.
+              As a web developer committed to providing exceptional digital
+              solutions, I bring a wide range of skills to the table
             </p>
             <article>
               <h3 class="AB_SubHead">Custom Web Design and Development</h3>
               <p>
-                We craft stunning, bespoke websites that blend captivating
-                design with seamless functionality. Our team meticulously
-                translates your vision into a pixel-perfect digital reality,
-                ensuring an immersive user experience and strong brand
-                representation.
+                Imagine a website that seamlessly blends stunning aesthetics
+                with effortless functionality. That's precisely what I bring to
+                the table. Using industry-leading design tools like Figma and
+                Adobe Design Tools, I create visually captivating user
+                interfaces that not only grab attention but also guide visitors
+                through an intuitive and immersive experience. Let's take your
+                brand's story and turn it into a beautifully designed digital
+                masterpiece.
               </p>
             </article>
 
             <article>
-              <h3 class="AB_SubHead">E-Commerce Solutions</h3>
+              <h3 class="AB_SubHead">
+                Front-end Development: Elevating User Engagement
+              </h3>
               <p>
-                Drive your online business forward with our robust and scalable
-                e-commerce solutions. We leverage industry-leading platforms and
-                technologies to build secure, user-friendly online stores that
-                optimize conversions and enhance customer satisfaction.
+                When it comes to front-end development, I'm more than just a
+                coder. I'm a storyteller. I utilize the power of JavaScript and
+                TypeScript, combined with my expertise in Vue.js and React.js,
+                to craft interactive and responsive websites that captivate your
+                audience from the very first click. Whether it's implementing
+                eye-catching animations or designing seamless transitions, I
+                focus on creating a user experience that leaves a lasting
+                impression.
               </p>
             </article>
 
             <article>
-              <h3 class="AB_SubHead">Web Application Development</h3>
+              <h3 class="AB_SubHead">Back-end Development</h3>
               <p>
-                We excel in developing powerful web applications that streamline
-                your business operations and deliver exceptional user
-                experiences. From sophisticated enterprise solutions to
-                innovative startups, our expertise spans across various
-                industries and technologies.
+                Behind every great website lies a robust and scalable back-end
+                infrastructure. With expertise in frameworks like Express.js,
+                Fastify, and Koa, I architect and develop powerful web
+                applications that bring your vision to life. From complex
+                database integrations to sophisticated logic and functionality,
+                I ensure that your website or application is not just
+                aesthetically pleasing, but also highly functional and
+                efficient.
               </p>
             </article>
 
             <article>
-              <h3 class="AB_SubHead">Graphics and Product Design</h3>
+              <h3 class="AB_SubHead">Servers and Hosting</h3>
               <p>
-                From visually striking logos to compelling marketing collateral,
-                our graphic design services help you create a cohesive and
-                impactful brand identity. We specialize in designing engaging
-                visuals that resonate with your target audience and leave a
-                lasting impression.
+                Your website deserves a reliable and secure home for your data.
+                I am well-versed in managing servers and hosting platforms such
+                as Google Cloud Platforms, AWS, and DigitalOcean. With an eye
+                for performance optimization and scalable architecture, I ensure
+                that your website delivers lightning-fast load times and
+                operates seamlessly, even under heavy traffic.
               </p>
             </article>
 
             <article>
-              <h3 class="AB_SubHead">Mobile-Friendly and Responsive Design</h3>
+              <h3 class="AB_SubHead">Version Control and Collaboration</h3>
               <p>
-                In today's mobile-centric world, we prioritize responsive design
-                to ensure your website looks flawless and performs seamlessly
-                across all devices. Our mobile-first approach guarantees an
-                optimal user experience, driving engagement and conversions.
+                To ensure efficient and collaborative development processes, I
+                utilize systems like GitHub, Basecamp, Discord, Slack, etc. This
+                allows for seamless code management, easy collaboration with
+                team members, and efficient project tracking.
               </p>
             </article>
 
             <article>
-              <h3 class="AB_SubHead">Search Engine Optimization (SEO)</h3>
+              <h3 class="AB_SubHead">
+                Blockchain Development: Embracing the Future
+              </h3>
               <p>
-                Our SEO experts employ proven strategies to optimize your
-                website for search engines, improving its visibility and driving
-                organic traffic. We focus on technical optimizations, content
-                strategies, and link building to enhance your online presence
-                and boost rankings.
+                The blockchain revolution is upon us, and I'm here to guide you
+                through it. With hands-on experience in Web3.js and Solidity, I
+                help businesses leverage the power of decentralized applications
+                and smart contracts. Whether it's building a secure and
+                transparent voting system or developing a unique
+                blockchain-based solution, I have the expertise to bring your
+                ideas to life on this transformative technology.
               </p>
             </article>
           </section>
 
           <section>
-            <h2 class="AB_Head">Our Approach</h2>
+            <h2 class="AB_Head">Still, Why You Should Choose Me?</h2>
             <ul>
               <li>
-                <strong class="t-blue-grey--1">Strategic Collaboration:</strong>
-                We believe in forging strong relationships with brands that
-                reach out to us. We work closely with you, engaging in open
-                communication and active collaboration throughout the entire
-                development process.
-              </li>
-              <li>
-                <strong class="t-blue-grey--1">Customized Solutions:</strong>
-                We understand that each client has unique requirements. That's
-                why we take a tailored approach to deliver solutions that align
-                with your specific goals and objectives.
-              </li>
-              <li>
-                <strong class="t-blue-grey--1">Strategic Guidance:</strong> We
-                offer objective guidance to address common challenges faced by
-                websites and digital businesses. Our team brings valuable
-                insights and expertise to the table, assisting you in making
-                informed decisions to maximize your online success.
+                <strong class="t-blue-grey--1"
+                  >Advanced Technical Skills:</strong
+                >
+                I stay up to date with the latest industry trends and
+                continuously expand my technical skill set. This allows me to
+                implement cutting-edge solutions and deliver high-quality
+                results that align with industry standards.
               </li>
               <li>
                 <strong class="t-blue-grey--1"
-                  >Innovation and Technology:</strong
-                >
-                We stay at the forefront of emerging technologies and industry
-                trends, enabling us to deliver solutions that are future-proof
-                and equipped with the latest advancements.
+                  >Analytical and Divergent Thinking:
+                </strong>
+                I possess strong problem-solving skills, enabling me to approach
+                challenges from different angles and provide innovative
+                solutions. I thrive on finding creative and efficient ways to
+                overcome obstacles and achieve optimal outcomes.
               </li>
               <li>
-                <strong class="t-blue-grey--1">Quality Assurance:</strong> Our
-                rigorous quality assurance processes ensure that every aspect of
-                your website or application is thoroughly tested, resulting in a
-                flawless user experience and optimal performance.
+                <strong class="t-blue-grey--1"
+                  >Effective Communication and Collaboration:</strong
+                >
+                When you choose to work with me, you're not just hiring a web
+                developer; you're gaining a trusted partner who is dedicated to
+                your success. I believe in effective communication,
+                transparency, and collaboration throughout the entire
+                development process. Your vision and goals are at the forefront
+                of every decision I make, ensuring that the end result aligns
+                with your expectations and possibly exceeds them.
               </li>
             </ul>
           </section>
 
           <section>
-            <h2 class="AB_Head">Let's Transform Your Online Presence</h2>
+            <h2 class="AB_Head">
+              Let's Create Something Extraordinary
+            </h2>
             <p>
-              Are you ready to elevate your brand's digital presence? Contact
-              our team today to discuss your web development goals. Together,
-              we'll embark on a journey to create a captivating online
-              experience that sets you apart from the competition and drives
-              measurable success.
+              Are you ready to elevate your brand's digital presence?
+              <NuxtLink to="hire-me"> Hire me</NuxtLink>. Get in touch with me
+              today, to discuss your web development goals. Together, we'll
+              embark on a journey to create a captivating online experience that
+              sets you apart from the competition and drives measurable success.
             </p>
           </section>
         </section>
