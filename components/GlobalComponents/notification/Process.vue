87acd167f10c74a6edc001281b4dbacef1c0198d
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-15 08:24:37 +0000

    Update Process.vue

--- components/GlobalComponents/notification/Process.vue
+++ components/GlobalComponents/notification/Process.vue
@@ -4,24 +4,14 @@
       <div v-show="status" class="Wrapper flex j-c-center noselect">
         <span class="Load bg-trans-1">
           <div
-            v-html="$appLogo('fill', 'logo-x-small logo-rotate shadow-')"
+            v-html="$appLogo('white', 'logo-x-small logo-rotate')"
           ></div>
           <div class="TextCont" :class="progressTotal > 0 ? 'HasProgress' : ''">
             <span
-              class="
-                bg-white
-                font-3
-                bold-3
-                px-1
-                py-1
-                br4
-                flex
-                a-i-center
-                j-c-around
-              "
+              class="font-3 bold-3 px-1 py-1 br4 flex a-i-center j-c-around"
             >
-              <span :class="icon" class="t-pink--4"></span>
-              <span class="Text t-pink--4 pr-2 letter-space-1">{{ info }}</span>
+              <span :class="icon"></span>
+              <span class="Text pr-2 letter-space-1">{{ info }}</span>
             </span>
             <i class="Progress" :style="'width:' + progressElapsed + '%;'"></i>
           </div>
@@ -31,35 +21,25 @@
   </div>
 </template>
 <script lang="ts">
-  import { useProcess } from "@/store";
+import { useProcess } from "@/store";
 
 export default {
   setup() {
     const $Process = useProcess();
-    
-     const status = computed(() =>
-      $Process.status
-    );
-     const info = computed(() =>
-      $Process.info
-    );
-     const icon = computed(() =>
-      $Process.icon
-    );
-     const progressTotal = computed(() =>
-      $Process.progressTotal
-    );
-     const progressElapsed = computed(() =>
-      $Process.progressElapsed
-    );
-    
+
+    const status = computed(() => $Process.status);
+    const info = computed(() => $Process.info);
+    const icon = computed(() => $Process.icon);
+    const progressTotal = computed(() => $Process.progressTotal);
+    const progressElapsed = computed(() => $Process.progressElapsed);
+
     return {
       status,
       info,
       icon,
       progressTotal,
-      progressElapsed
-    }
+      progressElapsed,
+    };
   },
 };
 </script>
@@ -78,23 +58,25 @@ export default {
   flex-wrap: nowrap;
   height: 55px;
   padding: 4px;
-  padding-bottom: 2px;
+  // padding-bottom: 2px;
   border-radius: 28px;
   & > .TextCont {
     min-width: 215px;
     max-width: 300px;
+    background-color: $sec-color;
+    color: $pri-color;
+    border-radius: 28px;
     &.HasProgress {
-      border-radius: 28px;
       height: 2.1em;
       position: relative;
-      background-color: white;
+      background-color: $sec-color;
       overflow: hidden;
       & > .Progress {
         position: absolute;
         top: 3px;
         bottom: 3px;
         width: 100%;
-        background-color: $pink;
+        background-color: $pri-color;
       }
       & > span {
         z-index: 1;
