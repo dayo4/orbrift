d32d123c301d86f1ce29cd726791e98975975dff
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-16 15:44:16 +0000

    attempting to include graphql

--- addons/utils/Axios.ts
+++ addons/utils/Axios.ts
@@ -6,9 +6,11 @@ const devMode = process.env.NODE_ENV === 'development'
 
 const $baseApi = axios.create({ baseURL: 'https://baseapi.orbrift.com/rift-v1/', })
 const $contentApi = axios.create({ baseURL: 'https://api.orbrift.com/wp-json/wp/v2/', })
+const $gqlApi = axios.create({ baseURL: `https://graphql.contentful.com/content/v1/spaces/fjmwmd7cfr3e/environments/_7nAj7xOmJ4ffOOaUQDEWDa69B7lyYLs2uQHwNADb5M`})
 
 // $baseApi.defaults.headers.common['Authorization'] = 'Bearer '
 // $contentApi.defaults.headers.common['Authorization'] = 'Bearer '
+$gqlApi.defaults.headers.common['Authorization'] = 'Bearer CFPAT-qjsAqPLCq6Z650ptnKmav-OxYXXXd1UpCNYSicUzsRE'
 
 $baseApi.interceptors.request.use(
   function (config) {
@@ -65,8 +67,36 @@ $contentApi.interceptors.response.use(
     return Promise.reject(response)
   }
 )
+$gqlApi.interceptors.request.use(
+  function (config) {
+    return config
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
+
+// Add a response interceptor
+$gqlApi.interceptors.response.use(
+  function (response) {
+    if (devMode)
+      console.log(response)
+
+    return response
+  },
+
+  /* if error */
+  function ({ response }) {
+    if (devMode)
+      console.log(response)
+
+    return Promise.reject(response)
+  }
+)
 
 export {
   $baseApi,
-  $contentApi
+  $contentApi,
+  $gqlApi
 }
