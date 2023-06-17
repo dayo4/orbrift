60214db87b7479a410141247da24a08fc2863764
dayo4 <dayo4@live.com>
2023-06-17 10:25:05 +0100

    edited

--- pages/test.vue
+++ pages/test.vue
@@ -2,34 +2,64 @@
   <div class="TestTop">
     <div>
       <h1>GraphQL Example</h1>
-      <ul v-if="posts">
-        {{
+      <!-- <ul v-if="posts"> -->
+      <!-- {{
           posts
-        }}
-      </ul>
+        }} -->
+      <!-- </ul> -->
     </div>
   </div>
-
 </template>
 <script lang="ts">
 import { usePosts } from "@/store";
 
 export default {
   setup() {
-    const $Posts = usePosts();
+    // const { clients } = useApollo()
+    // const $Posts = usePosts();
 
-    const {
-      data: posts,
-      error,
-      pending,
-    } = useAsyncData(async () => {
-      const res = await $Posts.gqlFetch();
-      console.log(res);
-      return res;
-    });
+    // const {
+    //   data: posts,
+    //   error,
+    //   pending,
+    // } = useAsyncData(async () => {
+    //   const res = await $Posts.gqlFetch();
+    //   console.log(res);
+    //   return res;
+    // });
 
+    // const query = gql`
+    //   query BlogPost {
+    //     blogPostCollection {
+    //       items {
+    //         title
+    //         slug
+    //         featuredMedia
+    //       }
+    //     }
+    //   }
+    // `;
+    // const query = gql`
+    //   query {
+    //     necCollection {
+    //       items {
+    //         title
+    //         sys {
+    //           id
+    //           publishedAt
+    //         }
+    //       }
+    //     }
+    //   }
+    // `;
+
+    // const { data } = useAsyncQuery(query, {
+    //   necId: "nec",
+    // });
+    // console.log(data);
     return {
-      posts,
+      // posts,
+      // data
     };
   },
 };
