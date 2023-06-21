42f4169ca16523a5126317241f18c82c24872f84
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-21 04:11:00 +0000

    Update about.vue and index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -58,8 +58,8 @@
         <span class="icon-check mr-2"></span>
         <span>My Approach.. </span>
       </h3>
-      <div class="Tiles bg-img-masked flex wrap j-c-center py-4">
-        <div class="xs11 sm10 md9 lg7 shadow-3" v-for="(L, i) in list" :key="i">
+      <div class="Tiles bg-img-masked flex wrap j-c-center py-4 noselect">
+        <article v-aos="'fade-up'" class="xs11 sm10 md9 lg7 shadow-3" v-for="(L, i) in list" :key="i">
           <div class="noselect">
             <img
               :src="L.link ? L.link : '/defaults/pgs/icons/' + L.img + '.jpg'"
@@ -67,9 +67,9 @@
               draggable="false"
             />
           </div>
-          <div>{{ L.text }}</div>
-          <div>{{ L.detail }}</div>
-        </div>
+          <h2>{{ L.text }}</h2>
+          <p>{{ L.detail }}</p>
+        </article>
       </div>
 
       <div class="flex j-c-center mt-6">
@@ -128,7 +128,7 @@
 
     <!-- FootNote   -->
     <section class="HomeFootNote mt-10">
-      <div data-aos="zoom-in" data-aos-once="true" class="flex j-c-center">
+      <div v-aos="'zoom-in'" class="flex j-c-center">
         <div class="xs12 md7 lg6">
           If you would love to have a more streamlined project discussion with
           me to request an estimate, use the button below
@@ -161,43 +161,57 @@ export default {
     const list = [
       {
         img: "resp",
-        text: "Pixel-perfect and Fully Responsive Webpages",
+        text: "Flawlessly Responsive Pages for Modern Devices",
         detail:
-          "My designs are expertly crafted with modern layouts, and you are delivered webpages that look and feel perfect on devices of various screen sizes.",
+          "Experience webpages that are carefully designed to adapt easily to any screen size. Whether your audience is browsing on a desktop, tablet, or mobile device, your apps will look stunning and provide an exceptional user experience.",
       },
       {
         img: "scale",
-        text: "Performant and Easily Scalable Apps",
+        text: "Performance-driven and Scalable Applications",
         detail:
-          "A successful web application should be reliable, well-planned and seamlessly accommodate growth. With me, Web Apps are designed to welcome subsequent growths without friction. And I technically avoid anything that can get my apps stuck in a hole.",
+          "With a focus on reliability and smart planning, I design and develop applications that are not only fast and efficient but also have the ability to scale effortlessly as your business grows. Say goodbye to bottlenecks.and hello to possibilities.",
       },
       {
         img: "",
-        text: "Concise Designs and Smooth Navigation",
+        text: "Intuitive Designs and Fluid Navigation",
         link: "/defaults/pgs/icons/clean.png",
         detail:
-          "Everything that makes up your webpages layouts, from designs to underlying frameworks are required to be well structured to improve your users experiences and engagement. With me you can be rest assured.",
+          "From the layout and aesthetics to the underlying framework, every aspect of your website and app is carefully crafted to ensure smooth navigation and enhance user engagement. ",
+      },
+      {
+        img: "flexb",
+        text: "Tailored Solutions that Capture Your Unique Identity",
+        // link: "/defaults/logo/orbrift-cy.png",
+        detail:
+          "Your website should reflect your brand's personality. Whether you prefer a sleek and minimalist design or a fully customized visual experience, I take the time to understand your needs and create a design that showcases the value of your products or services effectively.",
       },
       {
         img: "",
-        text: "Easy and Secure Project Management",
+        text: "Agile Development Approach",
         link: "/defaults/pgs/icons/secure.png",
         detail:
-          "It is my utmost priority to ensure that you get the right value for your input. Easy to use tools are employed to make the development, payment and communication process a breeze for your projects.",
+          "Through an agile development methodology, I deliver iterative solutions that evolve with your requirements. This approach allows for flexibility and quick adaptations on your software development projects.",
       },
-      // { img: 'flexb', text: 'Flexible Business Logic Implementation' },
-      // {
-      //   img: "effect",
-      //   text: "Effective Development and Customer Service",
-      //   detail:
-      //     "It is orbrift's utmost priority to ensure that you get the right value for your input.",
-      // },
       {
         img: "",
-        text: "Genuine Design Customized To Your Need",
-        link: "/defaults/logo/orbrift-cy.png",
+        text: "Integration with Third-Party Services",
+        link: "/defaults/pgs/icons/secure.png",
         detail:
-          "Some people desire minimalist designs and layouts while others want it outright customized. Regardless of your requirement, I am versed as presenting you with a design that simply shows the value of your products or services.",
+          "Maximize the functionality of your apps by easily integrating it with third-party services. From e-commerce platforms to payment gateways and CRM systems, I ensure smooth integration to enhance your users experience and business operations.",
+      },
+      {
+        img: "",
+        text: "Streamlined Project Management",
+        link: "/defaults/pgs/icons/secure.png",
+        detail:
+          "I employ user-friendly tools and streamlined processes to make communication and payment hassle-free. You can focus on your business while I handle the technicalities, ensuring a secure and efficient workflow.",
+      },
+      {
+        img: "",
+        text: "Continuous Support and Maintenance",
+        link: "/defaults/pgs/icons/secure.png",
+        detail:
+          "I provide ongoing support and maintenance services to keep your website or application running smoothly. From updates and security enhancements to performance optimizations, I ensure your digital presence remains robust and up-to-date.",
       },
     ];
 
@@ -318,14 +332,14 @@ export default {
     background-color: $sec-color;
     background-attachment: fixed;
     background-image: url("/defaults/pgs/orbrift_pfbg.jpg");
-    & > div {
+    & > article {
       z-index: 2;
       min-height: 330px;
       background-color: $light-color;
       border-radius: 28px;
       margin: 0px 30px 30px 20px;
-      border-top: solid 3px $sec-color;
-      border-bottom: solid 3px $sec-color;
+      border-top: solid 3px $pri-color;
+      border-bottom: solid 3px $pri-color;
       & img {
         width: 90px;
         height: 90px;
@@ -339,7 +353,7 @@ export default {
         height: 150px;
       }
 
-      & > div:nth-child(2) {
+      & > h2 {
         padding: 12px 5px;
         text-align: center;
         font-weight: bold;
@@ -350,7 +364,7 @@ export default {
         // background-color: #0A2647;
         // @include bg-gradient-2;
       }
-      & > div:last-child {
+      & > p {
         padding: 10px 10px 15px 10px;
         text-align: center;
         font-weight: bold;
@@ -396,35 +410,21 @@ export default {
           font-size: 28px;
         }
       }
-      & .More {
-        margin-left: 40px;
-      }
     }
   }
 
   .Section_2 {
     & .Tiles {
-      & > div {
+      & > article {
         margin: 0px 8px 30px 8px;
       }
     }
   }
 }
 @include xxs-only {
-  .Top {
-    .Detail {
-      & .More {
-        margin-left: 20px;
-        & > div:nth-child(1) {
-          margin-top: 3px;
-          font-size: 18px !important;
-        }
-      }
-    }
-  }
   .Section_2 {
     & .Tiles {
-      & > div {
+      & > article {
         flex: 0 0 91.66%;
         max-width: 91.66%;
       }
