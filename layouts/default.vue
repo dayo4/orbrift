18f26a61c1eb972543bc9a6f50c9b4df0f5e375f
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-21 04:10:47 +0000

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div
-      class="GBLMContainer"
-    >
+    <div class="GBLMContainer">
       <TopNav @search="searchOn = !searchOn" />
       <HoverPanel />
       <Process />
 
-      <Search v-show="searchOn" @searchoff="searchOn = false" />
+      <ClientOnly>
+        <Search v-show="searchOn" @searchoff="searchOn = false" />
+      </ClientOnly>
 
       <section class="Routes">
         <transition name="slide-fade" mode="out-in">
@@ -22,15 +22,13 @@
 </template>
 
 <script lang="ts">
-
 import { $General } from "@/addons";
 
 import { useNavs } from "@/store";
 
 export default {
-
   setup() {
-        const { $aos } = useNuxtApp();
+    const { $aos } = useNuxtApp();
 
     const $Navs = useNavs();
     const searchOn = ref(false);
@@ -47,7 +45,7 @@ export default {
         .catch((e) => {});
     });
 
-onMounted(()=>$aos())
+    onMounted(() => $aos());
 
     onUnmounted(() => {
       $Navs.removeScrollEvent();
@@ -61,13 +59,15 @@ onMounted(()=>$aos())
 </script>
 
 <style lang="scss">
-.slide-fade-leave-active, .slide-fade-enter-active {
-    transition: all 0.3s ease-in-out;
+.slide-fade-leave-active,
+.slide-fade-enter-active {
+  transition: all 0.3s ease-in-out;
 }
 
-.slide-fade-enter-from, .slide-fade-leave-to {
-    transform: translateX(20px);
-    opacity: 0;
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 .GBLMContainer {
   display: table;
