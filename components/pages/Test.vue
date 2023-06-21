b03673d529a4b4a3644a62a057b9ccb5c0652a8e
Adedayo <37295919+dayo4@users.noreply.github.com>
2023-06-21 04:07:48 +0000

    Update Test.vue and test.vue

---
+++ components/pages/Test.vue
@@ -0,0 +1,47 @@
+<template>
+
+
+</template>
+
+<script>
+export default {
+  setup() {
+    const icons = [
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+      "/defaults/logo/orbrift-cy.png",
+    ];
+    return {
+      icons,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
