9def428797601f1c1e7a65a29e76c4a38b3ec4f7
Samuel A. Adeniyi <dayo4@live.com>
2023-06-23 05:01:07 +0000

    modify techslider

--- components/pages/Test.vue
+++ components/pages/Test.vue
@@ -1,47 +1,126 @@
 <template>
-
-
+  <div class="Wrapr">
+    <div class="Main">
+      <div v-for="(icon, i) in icons" :key="i" class="Item">
+        <object class="SVG" :data="'/defaults/stk/svg/' + icon + '.svg'" type="image/svg+xml"></object>
+      </div>
+    </div>
+    <div class="Deco" aria-hidden="true">
+      <div v-for="(icon, i) in icons" :key="i" class="Item">
+        <object class="SVG" :data="'/defaults/stk/svg/' + icon + '.svg'" type="image/svg+xml"></object>
+      </div>
+    </div>
+  </div>
+  <!-- <Test/> -->
 </template>
-
-<script>
+  
+<script lang="ts">
 export default {
+
   setup() {
     const icons = [
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
-      "/defaults/logo/orbrift-cy.png",
+      "javascript",
+      "react-native",
+      "typescript",
+      "vue",
+      "nuxt",
+      "next",
+      "nodejs",
+      "google-cloud",
+      "graphql",
+      "firebase",
+      "vercel",
+      "wordpress",
+      "mysql",
+      "ember",
     ];
     return {
-      icons,
+      icons
     };
   },
 };
 </script>
+  
+<style lang="scss" scoped>
+.Wrapr {
+  overflow: hidden;
+  position: relative;
+}
 
-<style scoped>
+.Main {
+  display: flex;
+  animation: main 20s linear infinite;
+}
+.Deco {
+  position: absolute;
+  top: 0;
+  right:0;
+  display: flex;
+  animation: deco 20s linear infinite;
+  animation-delay: 5s;
+}
 
+.Item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 0 0 14vw;
+  height: 70px;
+  padding: 5px 0;
+  margin: 0 1vw;
+}
+
+.SVG {
+  display: block;
+  height: 100%;
+  max-width: 100%;
+}
+
+@keyframes main {
+  0% {
+    transform: translateX(112vw);
+  }
+
+  100% {
+    transform: translatex(-336vw);
+  }
+}
+
+@keyframes deco {
+  0% {
+    transform: translateX(224vw);
+  }
+
+  100% {
+    transform: translatex(-224vw);
+  }
+}
+
+@include xs-only {
+.Item {
+  flex: 0 0 18vw ;
+  margin: 0 2vw;
+}
+
+@keyframes main {
+  0% {
+    transform: translateX(154vw);
+  }
+
+  100% {
+    transform: translatex(-462vw);
+  }
+}
+
+@keyframes deco {
+  0% {
+    transform: translateX(308vw);
+  }
+
+  100% {
+    transform: translatex(-308vw);
+  }
+}
+}
 </style>
+  
--- pages/portfolio/[slug].vue
+++ pages/portfolio/[slug].vue
@@ -1,52 +1,39 @@
 <template>
   <GlobalWrapper :subText="false">
     <ClientOnly>
-    <template #Title> Portfolio </template>
-    <template #SubText> A Collection of Some of my Projects </template>
+      <template #Title> Portfolio </template>
+      <template #SubText> A Collection of Some of my Projects </template>
 
-    <!-- ListOfPosts Component -->
-    <template #WrapperBody>
-      <div v-if="project">
-        <section class="Projs">
-          <article
-            class="Proj xs12 sm10 md7"
-          >
-            <div class="TopSect">
-              <div class="ImgWrap">
-                <img
-                  :src="project.images?.items[0].url"
-                  :alt="project.images?.items[0].title"
-                  class="ProjImg"
-                  draggable="false"
-                />
+      <!-- ListOfPosts Component -->
+      <template #WrapperBody>
+        <div v-if="project">
+          <section class="Projs">
+            <article class="Proj xs12 sm10 md7">
+              <div class="TopSect">
+                <div class="ImgWrap">
+                  <img :src="project.images?.items[0].url" :alt="project.images?.items[0].title" class="ProjImg"
+                    draggable="false" />
+                </div>
+                <h2 class="Title">
+                  {{ project.title }}
+                </h2>
               </div>
-              <h2 class="Title">
-                {{ project.title }}
-              </h2>
-            </div>
-            <div class="flex j-c-center a-i-center">
-              <div v-for="(tech, i) in project.techs" :key="i" class="Techs">
-                <object
-                  class="SVG"
-                  :data="'defaults/stk/svg/' + tech + '.svg'"
-                  type="image/svg+xml"
-                ></object>
+              <div class="flex j-c-center a-i-center">
+                <div v-for="(tech, i) in project.techs" :key="i" class="Techs">
+                  <object class="SVG" :data="'defaults/stk/svg/' + tech + '.svg'" type="image/svg+xml"></object>
+                </div>
               </div>
-            </div>
-          </article>
-        </section>
-      </div>
-
-              <!-- BODY -->
-              <div class="flex j-c-center">
-        <section
-          v-html="parsedContent"
-          class="Body xs12 sm10 md7 p-5"
-        ></section>
+            </article>
+          </section>
         </div>
 
-    </template>
-  </ClientOnly>
+        <!-- BODY -->
+        <div class="flex j-c-center">
+          <section v-html="parsedContent" class="Body xs12 sm10 md7 p-5"></section>
+        </div>
+
+      </template>
+    </ClientOnly>
   </GlobalWrapper>
 </template>
 <script lang="ts">
@@ -117,18 +104,22 @@ export default {
 }
 
 .TopSect {
-  background-color: #333; /* Dark background color for the featured image and title */
+  background-color: #333;
+  /* Dark background color for the featured image and title */
   background-color: $sec-color-trans;
   border-radius: 4px;
   padding: 10px;
   margin-bottom: 10px;
   cursor: pointer;
 }
+
 .ImgWrap {
   width: 100%;
-  padding-top: 56.25%; /* 16:9 aspect ratio (9 divided by 16, multiplied by 100) */
+  padding-top: 56.25%;
+  /* 16:9 aspect ratio (9 divided by 16, multiplied by 100) */
   position: relative;
 }
+
 .ProjImg {
   position: absolute;
   top: 0;
@@ -138,6 +129,7 @@ export default {
   object-fit: cover;
   border-radius: 4px;
 }
+
 .Title {
   margin-top: 10px;
   margin-bottom: 10px;
@@ -152,6 +144,7 @@ export default {
   // border: solid 3px $pri-color;
   // border-radius: 50%;
   margin: 0 5px;
+
   & .SVG {
     width: 100%;
     height: 100%;
@@ -176,13 +169,16 @@ export default {
   border-bottom: rgb(45, 45, 45) solid 2px;
   border-top: rgb(45, 45, 45) solid 2px;
   border-radius: 10px;
+
   & button {
     box-shadow: none;
   }
+
   & .Nums {
     padding: 0;
     box-shadow: none;
     text-align: center;
+
     & span {
       border-radius: 4px;
       border-top: $grey solid 1px;
@@ -190,9 +186,11 @@ export default {
       color: $blue-grey--2;
       padding: 5px 8px;
       margin: 0 5px;
+
       &:hover {
         background-color: $grey-3;
       }
+
       &.active {
         background-color: $grey-2;
       }
