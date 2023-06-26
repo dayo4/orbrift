f9515c46e2fece1cf3d60a0cb18e33b9099282d5
dayo4 <dayo4@live.com>
2023-06-26 14:42:55 +0100

    update Axios.ts

--- addons/utils/Axios.ts
+++ addons/utils/Axios.ts
@@ -5,10 +5,8 @@ import axios from "axios";
 const devMode = process.env.NODE_ENV === 'development'
 
 const $baseApi = axios.create({ baseURL: 'https://baseapi.orbrift.com/rift-v1/', })
-const $contentApi = axios.create({ baseURL: 'https://api.orbrift.com/wp-json/wp/v2/', })
 
 // $baseApi.defaults.headers.common['Authorization'] = 'Bearer '
-// $contentApi.defaults.headers.common['Authorization'] = 'Bearer '
 
 $baseApi.interceptors.request.use(
   function (config) {
@@ -38,35 +36,7 @@ $baseApi.interceptors.response.use(
   }
 )
 
-$contentApi.interceptors.request.use(
-  function (config) {
-    return config
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error)
-  }
-)
-
-// Add a response interceptor
-$contentApi.interceptors.response.use(
-  function (response) {
-    if (devMode)
-      console.log(response)
-
-    return response
-  },
-
-  /* if error */
-  function ({ response }) {
-    if (devMode)
-      console.log(response)
-
-    return Promise.reject(response)
-  }
-)
 
 export {
-  $baseApi,
-  $contentApi,
+  $baseApi
 }
