e12dea2dc487be880af239f368b1e81d8a4db608
dayo4 <dayo4@live.com>
2023-07-01 08:45:08 +0000

    Jul 1, 2023, 9:45 AM

--- components/pages/TechStackSlider.vue
+++ components/pages/TechStackSlider.vue
@@ -91,7 +91,7 @@ export default {
   right: 0;
   display: flex;
   animation: deco 40s linear infinite;
-  animation-delay: 13s;
+  animation-delay: 12.7s;
   &.Invis {
     visibility: hidden;
     // display: none;
