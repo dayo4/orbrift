d3c599de4d74885323eebd30e18d54d2ef03d41d
dayo4 <dayo4@live.com>
2023-07-01 08:11:56 +0000

    Jul 1, 2023, 9:11 AM

--- components/pages/TechStackSlider.vue
+++ components/pages/TechStackSlider.vue
@@ -137,19 +137,19 @@ export default {
   }
 
   100% {
-    transform: translatex(-336vw);
+    transform: translatex(-300vw);
     // transform: translatex(-336vw);
   }
 }
 
 @keyframes deco {
   0% {
-    transform: translateX(224vw);
+    transform: translateX(200vw);
     // transform: translateX(224vw);
   }
 
   100% {
-    transform: translatex(-224vw);
+    transform: translatex(-200vw);
     // transform: translatex(-224vw);
   }
 }
