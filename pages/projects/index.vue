4a85fa8180f083161e2fd21525a5f9bc06561a9a
dayo4 <dayo4@live.com>
2023-07-05 14:36:45 +0000

    Jul 5, 2023, 3:36 PM

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -202,7 +202,7 @@ export default {
   padding: 6px 10px;
   cursor: pointer;
   outline: none;
-  color: white;
+  color: rgba(90, 90, 90, 0.295);
   font-weight: bold;
   border-radius: 4px;
   box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
