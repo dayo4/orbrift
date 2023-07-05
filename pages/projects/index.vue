5fc6760fdb8a0688de5ca189b8ca55eeaafea0c0
dayo4 <dayo4@live.com>
2023-07-05 14:45:39 +0000

    Jul 5, 2023, 3:45 PM

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -123,7 +123,7 @@ export default {
     });
 
     const openProject = (slug: string) => {
-      if(String(slug).startsWith()){
+      if(String(slug).startsWith("")){
         
       }
       router.push({ path: "/projects/" + slug });
