9939fe3d8655a78e886f21b165bd464e01ebf242
dayo4 <dayo4@live.com>
2023-07-05 15:48:36 +0000

    Jul 5, 2023, 4:48 PM

--- components/pages/MainContact.vue
+++ components/pages/MainContact.vue
@@ -114,12 +114,7 @@ export default {
 
     const { email_err, name_err, msg_err } = toRefs(errorRefs);
 
-    let error = $Mailer.error;
-    const error = computed(() => {
-      if (data.value) {
-        return data.value.project.items[0];
-      }
-    });
+    const error = computed(() => $Mailer.error);
     let success = $Mailer.success;
 
     const setMsg = (e: any) => {
