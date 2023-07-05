28b5e50542f2546a537c9015ae6ebd1d4a607716
dayo4 <dayo4@live.com>
2023-07-05 15:37:17 +0000

    Jul 5, 2023, 4:37 PM

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -9,7 +9,7 @@
       <div v-if="project">
         <section class="Projs">
           <article class="Proj xs12 sm9 md7 lg6">
-            <div class="TopSect">
+            <div class="TopSect noselect">
               <div class="ImgWrap">
                 <img
                   :src="project.images?.items[0].url"
