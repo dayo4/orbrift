47beeec92d8a75c283b6f3b998958ede68717ef1
dayo4 <dayo4@live.com>
2023-07-05 14:39:10 +0000

    Jul 5, 2023, 3:39 PM

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -208,7 +208,7 @@ export default {
   box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
     0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
   /* shadow-3 */
-  background-color: #41414160;
+  background-color: $sec-color;
   /* cyan--3 */
   transition: 0.1s;
 }
