70a5f1be28b7fe17669f6f1a931ccd1b4093f37d
Samuel A. Adeniyi <dayo4@live.com>
2023-07-08 21:01:45 +0000

    Jul 8, 2023, 9:01 PM

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -4,66 +4,58 @@
 
     <template #WrapperBody>
       <section class="flex j-c-center">
-      <div class="xs12 sm12 md8 px-5">
-      <h4 class="t-blue-grey--2">
-        We are committed to protecting the privacy of those who visit this
-        ORBRIFT Website and/or use our services.
-      </h4>
+        <div class="xs12 sm12 md8 px-5">
+          <h4 class="t-blue-grey--2">
+            We are committed to protecting the privacy of those who visit this
+            ORBRIFT Website and/or use our services.
+          </h4>
 
-      <i
-        >Any adjustment to this statement will be updated to this page and will
-        be effective when posted. Your continued use of this site following the
-        posting of any adjustment, modification, or change shall constitute your
-        acceptance.</i
-      >
+          <i>Any adjustment to this statement will be updated to this page and will
+            be effective when posted. Your continued use of this site following the
+            posting of any adjustment, modification, or change shall constitute your
+            acceptance.</i>
 
-      <h2>Information Usage and Disclosure</h2>
+          <h2>Information Usage and Disclosure</h2>
 
-      <h3>Information Collected</h3>
+          <h3>Information Collected</h3>
 
-      <p>
-        When you visit and interact with orbrift.com and you provide certain
-        information: personal information that you knowingly choose to disclose.
-        These information is used to process your requests and whatever peculiar
-        purpose for disseminating it accordingly..
-      </p>
+          <p>
+            When you visit and interact with orbrift.com and you provide certain
+            information: personal information that you knowingly choose to disclose.
+            These information is used to process your requests and whatever peculiar
+            purpose for disseminating it accordingly..
+          </p>
 
-      <h3>When Submitting Forms or Sending Mail</h3>
+          <h3>When Submitting Forms or Sending Mail</h3>
 
-      <p>
-        When you submit a form, you choose to provide us with your personal
-        information and information about your project if you choose to. You
-        provide certain personal information, such as your name, and email
-        address. At this time, the most information you would be required to
-        submit is your name, email address and the details of your message/request which
-        may or may not contain project or personal information.
-      </p>
-      <p>
-        Any information received during interaction will be used to handle your
-        requests and utilized towards engagement with your project by those
-        directly involved in the development process.
-      </p>
+          <p>
+            When you submit a form, you choose to provide us with your personal
+            information and information about your project if you choose to. You
+            provide certain personal information, such as your name, and email
+            address. At this time, the most information you would be required to
+            submit is your name, email address and the details of your message/request which
+            may or may not contain project or personal information.
+          </p>
+          <p>
+            Any information received during interaction will be used to handle your
+            requests and utilized towards engagement with your project by those
+            directly involved in the development process.
+          </p>
 
-      <h2>Data Storage and Use of Cookies</h2>
-      <p>
-        When you visit and interact with orbrift.com, for you to have a better
-        experience, certain information may be temporarily stored on your
-        computer.
-      </p>
-      <p>
-        Googles services like analytics and reCAPTCHA used on this site rely on
-        cookies. For more information on this stuffs kindly refer to the
-        <a
-          href="https://www.google.com/intl/en/policies/privacy/"
-          target="_blank"
-          >Google's privacy-policy</a
-        >
-        and
-        <a href="https://www.google.com/intl/en/policies/terms/" target="_blank"
-          >Terms</a
-        >
-      </p>
-      </div>
+          <h2>Data Storage and Use of Cookies</h2>
+          <p>
+            When you visit and interact with orbrift.com, for you to have a better
+            experience, certain information may be temporarily stored on your
+            computer.
+          </p>
+          <p>
+            Googles services like analytics and reCAPTCHA used on this site rely on
+            cookies. For more information on this stuffs kindly refer to the
+            <a href="https://www.google.com/intl/en/policies/privacy/" target="_blank">Google's privacy-policy</a>
+            and
+            <a href="https://www.google.com/intl/en/policies/terms/" target="_blank">Terms</a>
+          </p>
+        </div>
       </section>
     </template>
   </GlobalWrapper>
