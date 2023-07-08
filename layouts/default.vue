da0fce5ae9b6119f6c14b8c18019a96db9658b2e
dayo4 <dayo4@live.com>
2023-07-08 12:07:58 +0000

    Jul 8, 2023, 1:07 PM

--- layouts/default.vue
+++ layouts/default.vue
@@ -99,8 +99,8 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url('/defaults/pgs/hexgold.png'); /* Replace '~/assets/background.jpg' with your image path */
-  //background-image: url('defaults/pgs/goldlines.png'); /* Replace '~/assets/background.jpg' with your image path */
+  // background-image: url('/defaults/pgs/hexgold.png'); /* Replace '~/assets/background.jpg' with your image path */
+  background-image: url('defaults/pgs/goldlines.png'); /* Replace '~/assets/background.jpg' with your image path */
   //background-image: url('defaults/pgs/patternbg.png'); /* Replace '~/assets/background.jpg' with your image path */
   background-size: contain;
   background-repeat: repeat;
