deed6f84142486c5a7f3d952e5dd524ec6e04b6b
dayo4 <dayo4@live.com>
2023-07-09 06:23:45 +0000

    Jul 9, 2023, 7:23 AM

--- pages/index.vue
+++ pages/index.vue
@@ -277,7 +277,7 @@ export default {
   position: relative;
   overflow: hidden;
   min-height: 500px;
-  width: 100vw;
+  width: 100%;
   padding-top: 60px;
   padding-bottom: 20px;
   // margin-bottom: 50px;
