7a362e73f279f44b1c2bc390473291bf07740667
dayo4 <dayo4@live.com>
2023-07-28 08:57:59 +0000

    Jul 28, 2023, 9:57 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="App">
-    <div ref="parallaxBg bg-img-masked" class="ParallaxBg"></div>
+    <div ref="parallaxBg" class="ParallaxBg bg-img-masked"></div>
 
     <div class="GBLMContainer">
       <TopNav @search="searchOn = !searchOn" />
