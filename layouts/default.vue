3bdd0c9d6303b2669dc2e209799a1148f8bb9b51
dayo4 <dayo4@live.com>
2023-07-28 09:02:37 +0000

    Jul 28, 2023, 10:02 AM

--- layouts/default.vue
+++ layouts/default.vue
@@ -114,7 +114,7 @@ export default {
     right: 0;
     left: 0;
     z-index: -1 !important;
-    background-color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(255, 255, 255, 0.836);
   }
 }
 </style>
