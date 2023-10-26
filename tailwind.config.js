78faa188675358291c0c9dd039909833830c25d6
dayo4 <dayo4@live.com>
2023-10-26 11:59:18 +0000

    added bg imgs and extra custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,35 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        xs: "320px",
         sm: "600px",
         md: "960px",
         lg: "1264px",
         xl: "1904px",
       },
       colors: {
-        "my-pri-color": 'rgb(231, 202, 125)',
-        "my-pri-color-trans": 'rgba(231, 202, 125, 0.9)',
-        "my-pri-color-trans-1": 'rgba(231, 202, 125, 0.75)',
-        "my-pri-color-trans-2": 'rgba(231, 202, 125, 0.5)',
-        "my-pri-color-trans-3": 'rgba(231, 202, 125, 0.25)',
+        "my-pri-color": "rgb(231, 202, 125)",
+        "my-pri-color-trans": "rgba(231, 202, 125, 0.9)",
+        "my-pri-color-trans-1": "rgba(231, 202, 125, 0.75)",
+        "my-pri-color-trans-2": "rgba(231, 202, 125, 0.5)",
+        "my-pri-color-trans-3": "rgba(231, 202, 125, 0.25)",
 
         // Secondary Color
-        "my-sec-color": 'rgb(39, 64, 96)',
-        "my-sec-color-trans": 'rgba(39, 64, 96, 0.9)',
-        "my-sec-color-trans-1": 'rgba(39, 64, 96, 0.75)',
-        "my-sec-color-trans-2": 'rgba(39, 64, 96, 0.5)',
-        "my-sec-color-trans-3": 'rgba(39, 64, 96, 0.25)',
+        "my-sec-color": "rgb(39, 64, 96)",
+        "my-sec-color-trans": "rgba(39, 64, 96, 0.9)",
+        "my-sec-color-trans-1": "rgba(39, 64, 96, 0.75)",
+        "my-sec-color-trans-2": "rgba(39, 64, 96, 0.5)",
+        "my-sec-color-trans-3": "rgba(39, 64, 96, 0.25)",
+
+        // Other Colors
+        "my-light-color" : "white",
+        "my-dark-color" : "black",
+        "my-dark-text-color" : "rgba(0, 0, 0, 0.65)"
+      },
+      backgroundImage: {
+        'home-hero-bg-img' : 'url("/defaults/pgs/orbrift_web_developer.jpg")',
+        'home-hero-user-img' : 'url("/defaults/pgs/dev.jpg")',
+        'home-tiles-bg-img' : 'url("/defaults/pgs/orbrift_web_developer2.jpg")'
       },
     },
   },
